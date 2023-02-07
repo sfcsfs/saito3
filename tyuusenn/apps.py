4de2223f42776883c612fdd27d12b81be71e21ad
sfcsfs <uhihihihishapemaybe618@gmail.com>
2023-02-08 05:52:04 +0900

    first commit

--- tyuusenn/apps.py
+++ tyuusenn/apps.py
@@ -2,12 +2,10 @@ from django.apps import AppConfig
 
 
 class TyuusennConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tyuusenn'
-
-
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tyuusenn"
 
     def ready(self):
         from .views import start
-        start()
 
+        start()
--- tyuusenn/test.py
+++ tyuusenn/test.py
@@ -1,28 +1,16 @@
-import matplotlib.pyplot as plt
-import base64
-from io import BytesIO
-import japanize_matplotlib
-#plt.rcParams["font.family"] = "MS Gothic"
-#プロットしたグラフを画像データとして出力するための関数
-def Output_Graph():
-	buffer = BytesIO()                   #バイナリI/O(画像や音声データを取り扱う際に利用)
-	plt.savefig(buffer, format="png")    #png形式の画像データを取り扱う
-	buffer.seek(0)                       #ストリーム先頭のoffset byteに変更
-	img   = buffer.getvalue()            #バッファの全内容を含むbytes
-	graph = base64.b64encode(img)        #画像ファイルをbase64でエンコード
-	graph = graph.decode("utf-8")        #デコードして文字列から画像に変換
-	buffer.close()
-	return graph
+from django.test import TestCase
+from django.urls import reverse
 
-#グラフをプロットするための関数
-def Plot_Graph(x,y):
-	plt.switch_backend("AGG")        #スクリプトを出力させない
-	plt.figure(figsize=(10,5))       #グラフサイズ
-	plt.bar(x,y)                     #グラフ作成
-	plt.xticks(rotation=45)          #X軸値を45度傾けて表示
-	plt.title("graph",fontsize=14)    #グラフタイトル
-	plt.xlabel("room number",fontsize=14)               #xラベル
-	plt.ylabel("number of votes",fontsize=14)       #yラベル
-	plt.tight_layout()               #レイアウト
-	graph = Output_Graph()           #グラフプロット
-	return graph
+# Create your tests here.
+class XTestCase(TestCase):
+    # HTTPステータスコードが200になるかどうか
+    def test_response_200(self):
+        x_url = reverse("tyuusenn:Login")
+        detail_response = self.client.get(x_url)
+        self.assertEqual(detail_response.status_code, 200)
+
+    # HTTPステータスコードが302になるかどうか
+    def test_response_302(self):
+        x_url = reverse("tyuusenn:Logout")
+        detail_response = self.client.get(x_url)
+        self.assertEqual(detail_response.status_code, 302)
